6b0ababbe7a748b5c851fdd83c09620a975f89e7
applegogo <gogocoming@163.com>
2017-04-27 13:49:15 +0800

    commit0426

---
+++ app/src/androidTest/java/com/example/apple/test50/TestClass.java
@@ -0,0 +1,13 @@
+package com.example.apple.test50;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * 作者：gogocoming on 17/4/11 16:54
+ * 邮箱：gogocoming@163.com
+ */
+public class TestClass extends InstrumentationTestCase {
+    public void test() throws  Exception{
+        assertEquals("测试不通过",2,3);
+    }
+}
